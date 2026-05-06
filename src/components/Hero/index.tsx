a1857631a732d26d6039c7d0dc19d8a0b2bd153c
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-05-06 20:01:55 -0300

    feat: add Button component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,4 +1,5 @@
 import Banner from "../../assets/images/hero/banner.svg";
+import { Button } from "../Button";
 
 export const Hero = () => {
   return (
@@ -11,14 +12,14 @@ export const Hero = () => {
             className="w-full h-full object-cover rounded-[20px]"
           />
 
-          <div className="absolute w-full bottom-0 flex flex-col justify-end items-center text-center px-6 md:px-24 pb-32">
+          <div className="absolute w-full bottom-0 flex justify-end items-center text-center px-6 md:px-24 pb-32">
             <div className="flex flex-col items-center text-white w-97">
                 <h2 className="text-xl font-medium leading-normal tracking-wider mb-2.5">Krypton One</h2>
                 <h1 className="text-2xl leading-9 tracking-widest mb-10">Transforme qualquer passo em presença</h1>
 
                 <div className="flex gap-2.5">
-                    <button>Ver modelos</button>
-                    <button>Comprar</button>
+                    <Button variant="secondary" size="sm">Ver modelos</Button>
+                    <Button>Comprar</Button>
                 </div>
             </div>
           </div>
