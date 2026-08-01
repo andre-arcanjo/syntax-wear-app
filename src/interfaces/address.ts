61fd238d87c1091a7561145907ebcbba91074c9d
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-08-01 15:03:22 -0300

    modify interface address

--- src/interfaces/address.ts
+++ src/interfaces/address.ts
@@ -1,16 +1,6 @@
 export interface Address {
-    cep: string;
-    logradouro: string;
-    complemento: string;
-    bairro: string;
-    unidade: string;
-    localidade: string;
-    uf: string;
-    ibge: string;
-    estado: string;
-    regiao: string;
-    gia: string;
-    ddd: string;
-    siafi: string;
-    shippingCost: number;
+  city: string;
+  state: string;
+  region: string;
+  shippingCost: number;
 }
